6a2eb75adef77a34eeacef4ccaeea5b3a248b2b2
Scott Williams <scottbwilliams.20@gmail.com>
2022-08-31 16:54:01 -0400

    application layers, repo pattern, testing the repo lessons

---
+++ week-03/lessons/explore-venus/src/test/java/learn/venus/data/OrbiterFileRepositoryTest.java
@@ -0,0 +1,64 @@
+package learn.venus.data;
+
+import learn.venus.models.Orbiter;
+import learn.venus.models.OrbiterType;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrbiterFileRepositoryTest {
+
+    private OrbiterFileRepository repository = new OrbiterFileRepository("./data/orbiters.csv");
+
+    @Test
+    void shouldFindFiveOrbiters() {
+        List<Orbiter> actual = repository.findAll();
+
+        assertNotNull(actual);
+        assertEquals(5,actual.size());
+
+    }
+
+    @Test
+    void shouldFindExistingOrbiter() {
+        Orbiter celestyn = repository.findById(4);
+
+        assertNotNull(celestyn);
+        assertEquals("Celestyn Kamien", celestyn.getName());
+    }
+
+    @Test
+    void shouldNotFindNonExistingOrbiter() {
+        Orbiter nope = repository.findById(10000);
+        assertNull(nope);
+    }
+
+    @Test
+    void shouldFineOneOfEachType() {
+        List<Orbiter> modules = repository.findByType(OrbiterType.MODULE);
+        assertNotNull(modules);
+        assertEquals(1,modules.size());
+
+        List<Orbiter> modulesWithDocks = repository.findByType(OrbiterType.MODULE_WITH_DOCK);
+        assertNotNull(modulesWithDocks);
+        assertEquals(1,modulesWithDocks.size());
+
+        List<Orbiter> shuttles = repository.findByType(OrbiterType.SHUTTLE);
+        assertNotNull(shuttles);
+        assertEquals(1,shuttles.size());
+
+        List<Orbiter> astronauts = repository.findByType(OrbiterType.ASTRONAUT);
+        assertNotNull(astronauts);
+        assertEquals(1,astronauts.size());
+
+        List<Orbiter> venusians = repository.findByType(OrbiterType.VENUSIAN);
+        assertNotNull(venusians);
+        assertEquals(1,venusians.size());
+
+    }
+
+
+
+}
