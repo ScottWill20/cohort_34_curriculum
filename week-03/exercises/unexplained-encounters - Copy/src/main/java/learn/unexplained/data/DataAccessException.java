11b3266a0bf764e3b4f7588632f308b6e4d8a5ee
Scott Williams <scottbwilliams.20@gmail.com>
2022-09-01 20:06:49 -0400

    unexplained encounters - domain ex complete

--- week-03/exercises/unexplained-encounters - Copy/src/main/java/learn/unexplained/data/DataAccessException.java
+++
@@ -1,8 +0,0 @@
-package learn.unexplained.data;
-
-public class DataAccessException extends Exception {
-
-    public DataAccessException(String message, Throwable rootCause) {
-        super(message, rootCause);
-    }
-}
