5f8a324f286fa8a53fb08270dd7351574b015270
Scott Williams <scottbwilliams.20@gmail.com>
2022-09-27 13:05:35 -0400

    findAll running needs revision

--- week-07/exercises/schema_jdbctemplate_exercise/solar-farm-starter/src/main/java/learn/solarfarm/App.java
+++ week-07/exercises/schema_jdbctemplate_exercise/solar-farm-starter/src/main/java/learn/solarfarm/App.java
@@ -1,17 +1,22 @@
 package learn.solarfarm;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import learn.solarfarm.data.DataAccessException;
 import learn.solarfarm.data.SolarPanelJdbcRepositoryTemplate;
 import learn.solarfarm.data.SolarPanelRepository;
 import learn.solarfarm.models.SolarPanel;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @ComponentScan
 public class App {
+
     public static void main(String[] args) throws DataAccessException {
         System.out.println("Welcome to Solar Farm!");
 
@@ -21,9 +26,26 @@ public class App {
         List<SolarPanel> panels = repository.findAll();
 
         for (SolarPanel solarPanel : panels) {
-            System.out.println(solarPanel);
+            System.out.printf("ID: %s section: %s row: %s column: %s Year Installed: %s Material: %s Is Tracking: %s %n", solarPanel.getId(), solarPanel.getSection(), solarPanel.getRow(), solarPanel.getColumn(), solarPanel.getYearInstalled(), solarPanel.getMaterial(), solarPanel.isTracking());
         }
 
+    }
+
+    // NOTE: THIS IS TEMPORARY !!!!!!!!!!!!! \\
+
+    @Bean
+    public DataSource getDataSource() {
+        MysqlDataSource result = new MysqlDataSource();
+        // Url is for the test database.
+        result.setUrl("jdbc:mysql://localhost:3306/solar_farm");
+        result.setUser("root");
+        result.setPassword("top-secret-password");
+        return result;
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
 }
