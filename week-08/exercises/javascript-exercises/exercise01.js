d0b228a13d5469cc7d856bc646cce45ef58a7b78
Scott Williams <scottbwilliams.20@gmail.com>
2022-10-04 15:05:07 -0400

    js exercises in progress

--- week-08/exercises/javascript-exercises/exercise01.js
+++ week-08/exercises/javascript-exercises/exercise01.js
@@ -8,8 +8,27 @@
 // - If none of the managers ask, print "Nothing to do..."
 
 const managerAAsked = true;
-const managerBAsked = true;
-const managerCAsked = true;
+const managerBAsked = false;
+const managerCAsked = false;
 
 // 1. Add decisions statements to cover all scenarios.
+
+if (!managerAAsked && !managerBAsked && !managerCAsked) {
+    console.log("Nothing to do...");
+} else if (managerAAsked && !managerBAsked && !managerCAsked) { // only A asked
+    console.log("Going to hold off for a bit.")
+} else if (!managerAAsked && managerBAsked && !managerCAsked) { // only B asked
+    console.log("Going to hold off for a bit.")
+} else if (!managerAAsked && !managerBAsked && managerCAsked) { // only C asked
+    console.log("Going to hold off for a bit.")
+} else if (managerAAsked && managerBAsked && !managerCAsked) { // A and B asked
+    console.log("Adding feature to schedule.")
+} else if (managerAAsked && !managerBAsked && managerCAsked) { // A and C asked
+    console.log("Adding feature to schedule.")
+} else if (!managerAAsked && managerBAsked && managerCAsked) { // B and C asked
+    console.log("Adding feature to schedule.")
+} else {
+    console.log("Feature in progress.")
+}
+
 // 2. Change manager variables to test all scenarios.
--- week-08/exercises/javascript-exercises/exercise02.js
+++ week-08/exercises/javascript-exercises/exercise02.js
@@ -20,12 +20,24 @@ final potters     | 16 | 21 | 10 | 11 | 55 |  5 |
 */
 
 // Change these numbers to test each scenario.
-let swimmers = 9;
-let archers = 10;
-let potters = 12;
+let swimmers = 11;
+let archers = 0;
+let potters = 11;
 
 // Write your distribution code here.
 // One set of code must work for all scenarios.
+
+if (swimmers != 2 % 0 && archers - swimmers >= potters) {
+    potters += swimmers;
+    swimmers = 0;
+} else {
+    const newArchers = Math.round(swimmers / 2);
+    const newPotters = Math.floor(swimmers / 2);
+
+    archers += newArchers;
+    potters += newPotters;
+    swimmers = 0;
+}
 
 // Confirm results for each scenario.
 console.log(`Swimmers: ${swimmers}`);
