a305f32dc662003f03e49fd4a8d604af5c12d66b
Scott Williams <scottbwilliams.20@gmail.com>
2022-10-05 16:57:15 -0400

    capsule hotel checkin and messages almost done

--- week-08/exercises/capsule-hotel/code.js
+++ week-08/exercises/capsule-hotel/code.js
@@ -13,3 +13,107 @@ function init() {
 }
 
 init();
+
+
+// *********************************
+
+// User Stories
+        // enter a guests name in Guest input and remember their name 
+        // enter a capsule number in "Capsule #" input and remember number
+        // On form submit...
+            // I want to take the save name and number and insert them
+            // in to the Capsules column in the correct place
+
+// Check Out Form
+    // enter a capsule # in the "Capsule #" input and remember it
+    // On form submit...
+        // find correct capsule in the capsule's column and
+        // set that column back to "Unoccupied"
+
+// Messages - Check In
+    // When book a guest is successful...
+        // display a success message
+    // when book a guest is unsuccessful...
+        // Validations
+            // already checked in
+            // empty name/capsule
+            // capsule already booked
+        // Unsuccessful message, according to our validations
+
+// Messages - Check Out
+    // when checkout is successful...
+        // display a success message
+    // when checkout is unsuccessful
+        // Validations
+            // already unoccupied
+            // empty name/capsule
+            // incorrect name/capsule combo
+            // no guest with that name
+
+// *********************************
+
+// Elements
+const bookForm = document.getElementById("book-guest-form")
+const bookGuestNameInput = document.getElementById("guest")
+const bookCapsuleInput = document.getElementById("bookingCapsule")
+const capsulesColumn = document.getElementById("capsules")
+const messagesContainer = document.getElementById("messages")
+
+// Global Variables
+let currentGuestName = bookGuestNameInput.value;
+let currentBookCapsule = bookCapsuleInput.value;
+
+
+// Helper Functions
+
+const handleMessageUpdate = (message) => {
+    messagesContainer.innerText = message;
+}
+
+// Functions
+
+const handlebookGuestNameChange = (event) => {
+    currentGuestName = event.target.value;
+}
+
+const handleBookCapsuleChange = (event) => {
+    currentBookCapsule = event.target.value;
+}
+
+const handleBookSubmit = (event) => {
+    event.preventDefault();
+    // need to find row with span#guest+currentBookCapsule
+
+    const capsuleRow = document.getElementById("guest" + currentBookCapsule);
+    const capsulePill = document.getElementById("capsuleLabel" + currentBookCapsule);
+
+    if (capsuleRow == null) {
+        handleMessageUpdate("ERROR: capsule does not exist.")
+    } else {
+        if (capsuleRow.innerText == "Unoccupied") {
+            if (currentGuestName == "") {
+                handleMessageUpdate("ERROR: Guest name cannot be blank.")
+            } 
+    
+            [...capsulesColumn.children].forEach(capsule => {
+                if (capsule.innerText.includes(currentGuestName)) {
+                    handleMessageUpdate("ERROR: Guest is already checked in.")
+                } 
+                else {
+                    capsuleRow.innerText = currentGuestName;
+                    capsulePill.style.backgroundColor = "#843edc";
+                    handleMessageUpdate(`SUCCESS: ${currentGuestName} is checked in!`)
+                }
+    
+            })
+        } else {
+            handleMessageUpdate("ERROR: This room is already occupied!")
+        }
+    }
+}
+
+// Events
+
+bookForm.addEventListener("submit", handleBookSubmit);
+bookGuestNameInput.addEventListener("change", handlebookGuestNameChange);
+bookCapsuleInput.addEventListener("change", handleBookCapsuleChange);
