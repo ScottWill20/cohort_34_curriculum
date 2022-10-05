9c292e4235399cd6d8d62ff72b56ff081b727ebf
Scott Williams <scottbwilliams.20@gmail.com>
2022-10-05 17:55:01 -0400

    capsule hotel exercise done

--- week-08/exercises/capsule-hotel/code.js
+++ week-08/exercises/capsule-hotel/code.js
@@ -1,3 +1,5 @@
+// Initialization
+
 const CAPSULE_COUNT = 100;
 
 function init() {
@@ -15,64 +17,66 @@ function init() {
 init();
 
 
-// *********************************
+// ==============================================
 
 // User Stories
-        // enter a guests name in Guest input and remember their name 
-        // enter a capsule number in "Capsule #" input and remember number
-        // On form submit...
-            // I want to take the save name and number and insert them
-            // in to the Capsules column in the correct place
+
+// Book a Guest Form
+    // Enter a guest's name in "Guest" input and remember their name ✔
+    // Enter a capsule number in "Capsule #" input and remember number ✔
+    // On form submit...
+        // I want to take the saved name and number and insert them ✔
+        // in to the Capsules column in the correct place ✔
 
 // Check Out Form
-    // enter a capsule # in the "Capsule #" input and remember it
+    // Enter a capsule number in the "Capsule #" input and remember it
     // On form submit...
-        // find correct capsule in the capsule's column and
+        // I want to find the correct Capsule in the Capsules column and
         // set that column back to "Unoccupied"
 
-// Messages - Check In
-    // When book a guest is successful...
-        // display a success message
-    // when book a guest is unsuccessful...
-        // Validations
-            // already checked in
-            // empty name/capsule
-            // capsule already booked
-        // Unsuccessful message, according to our validations
+// Messages
+    // For checking in...
+        // When Book a Guest is successful...
+            // Display a success message ✔
+        // When Book a Guest is not successful...
+            // VALIDATIONS
+                // Already checked in ✔
+                // Name OR Capsule is blank ✔
+                // Already Occupied ✔
+                // Capsule does not exist ✔
+            // Unsuccessful message, reason why according to validation ✔
+    // For checking out...
+        // When checkout is successful...
+            // Display a success message
+        // When checkout is not successful...
+            // VALIDATIONS
+                // Capsule is blank
+                // Already unoccupied
+                // Capsule does not exist
+            // Unsuccessful message, reason why according to validation
 
-// Messages - Check Out
-    // when checkout is successful...
-        // display a success message
-    // when checkout is unsuccessful
-        // Validations
-            // already unoccupied
-            // empty name/capsule
-            // incorrect name/capsule combo
-            // no guest with that name
-
-// *********************************
+// ==============================================
 
 // Elements
-const bookForm = document.getElementById("book-guest-form")
-const bookGuestNameInput = document.getElementById("guest")
-const bookCapsuleInput = document.getElementById("bookingCapsule")
-const capsulesColumn = document.getElementById("capsules")
-const messagesContainer = document.getElementById("messages")
+const bookForm = document.getElementById("book-guest-form");
+const checkOutForm = document.getElementById("checkout-guest-form")
+const bookGuestNameInput = document.getElementById("guest");
+const bookCapsuleInput = document.getElementById("bookingCapsule");
+const capsulesColumn = document.getElementById("capsules");
+const messagesContainer = document.getElementById("messages");
 
 // Global Variables
 let currentGuestName = bookGuestNameInput.value;
 let currentBookCapsule = bookCapsuleInput.value;
 
-
-// Helper Functions
+// Helper Function
 
 const handleMessageUpdate = (message) => {
     messagesContainer.innerText = message;
 }
 
 // Functions
-
-const handlebookGuestNameChange = (event) => {
+const handleBookGuestNameChange = (event) => {
     currentGuestName = event.target.value;
 }
 
@@ -82,38 +86,66 @@ const handleBookCapsuleChange = (event) => {
 
 const handleBookSubmit = (event) => {
     event.preventDefault();
-    // need to find row with span#guest+currentBookCapsule
 
     const capsuleRow = document.getElementById("guest" + currentBookCapsule);
     const capsulePill = document.getElementById("capsuleLabel" + currentBookCapsule);
 
     if (capsuleRow == null) {
-        handleMessageUpdate("ERROR: capsule does not exist.")
+        handleMessageUpdate("ERROR: Capsule is not in range.");
     } else {
         if (capsuleRow.innerText == "Unoccupied") {
-            if (currentGuestName == "") {
-                handleMessageUpdate("ERROR: Guest name cannot be blank.")
-            } 
     
             [...capsulesColumn.children].forEach(capsule => {
-                if (capsule.innerText.includes(currentGuestName)) {
-                    handleMessageUpdate("ERROR: Guest is already checked in.")
+
+                if (currentGuestName == "") {
+                    handleMessageUpdate("ERROR: Guest name cannot be blank.");
                 } 
+                
+                else if (capsule.innerText.includes(currentGuestName)) {
+                    handleMessageUpdate(`ERROR: ${currentGuestName} is already checked in.`);
+                }
                 else {
                     capsuleRow.innerText = currentGuestName;
-                    capsulePill.style.backgroundColor = "#843edc";
-                    handleMessageUpdate(`SUCCESS: ${currentGuestName} is checked in!`)
+                    capsulePill.style.backgroundColor = "#E54242";
+                    handleMessageUpdate(`SUCCESS: ${currentGuestName} is checked in!`);
                 }
-    
             })
+    
         } else {
-            handleMessageUpdate("ERROR: This room is already occupied!")
+            handleMessageUpdate("ERROR: This room is already occupied.");
         }
     }
+
 }
 
-// Events
+const handleCheckoutSubmit = (event) => {
+    event.preventDefault();
 
-bookForm.addEventListener("submit", handleBookSubmit);
-bookGuestNameInput.addEventListener("change", handlebookGuestNameChange);
+    const capsuleRow = document.getElementById("guest" + currentBookCapsule);
+    const capsulePill = document.getElementById("capsuleLabel" + currentBookCapsule);
+
+    if (capsuleRow == null) {
+        handleMessageUpdate("Error: Capsule is not in range.");
+    } else {
+
+        if (capsuleRow.innerText == "Unoccupied") {
+            handleMessageUpdate("Error: Capsule is unoccupied.");
+        } else {
+            capsuleRow.innerText = "Unoccupied";
+            capsulePill.style.backgroundColor = "#28a745";
+            handleMessageUpdate(`SUCCESS: ${currentGuestName} checked out of capsule #${currentBookCapsule}!`);
+        }
+    }
+    
+}
+
+
+// Events
+bookGuestNameInput.addEventListener("change", handleBookGuestNameChange);
 bookCapsuleInput.addEventListener("change", handleBookCapsuleChange);
+bookForm.addEventListener("submit", handleBookSubmit);
+checkOutForm.addEventListener("submit", handleCheckoutSubmit)
+
+// QUESTIONS \\
+// why dont we need an EventListener for messagesContainer?
+
