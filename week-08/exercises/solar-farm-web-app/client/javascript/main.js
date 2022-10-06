9890e11637c649a9b1bae0217427ff21e6bdefd4
Scott Williams <scottbwilliams.20@gmail.com>
2022-10-06 16:52:43 -0400

    solar-farm client exercise

--- week-08/exercises/solar-farm-web-app/client/javascript/main.js
+++ week-08/exercises/solar-farm-web-app/client/javascript/main.js
@@ -1,38 +1,7 @@
 const endpoint = 'http://localhost:8080/api/solarpanel';
-// fetch(endpoint) // this returns a promise
-
-// .then(response => {
-//     return response.json(); // this returns another promise
-// }).then(data => {
-//     console.log(data);
-//     const tableBodyElement = document.getElementById('tableRows');
-
-//     const solarPanelsHTML = data.map(sp =>{
-//         return `
-//         <tr>
-//         <td>${sp.section}</td>
-//         <td>${sp.row} - ${sp.column}</td>
-//         <td>${sp.yearInstalled}</td>
-//         <td>${sp.material}</td>
-//         <td>${sp.tracking}</td>
-//         <td>
-//             <button>Edit</button>
-//             <button>Delete</button>
-//         </td>
-//     <tr>
-//     `
-//     });
-
-//     console.log(solarPanelsHTML);
-
-//     tableBodyElement.innerHTML = solarPanelsHTML.join(''); // takes all the elements in an array and combines them as a string
-
-// });
-
 
 function init(){
     getSolarPanels().then(data => renderList(data));
-
 }
 
 init();
@@ -47,6 +16,7 @@ function getSolarPanels() {
 
 function renderList(solarPanels) {
     // will render my panels from the API
+
     const solarPanelsHTML = solarPanels.map(sp =>{
         return `
         <tr>
