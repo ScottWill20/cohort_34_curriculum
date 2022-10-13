9409bbd317f720d62431dbf018ffc803354c8cd6
Scott Williams <scottbwilliams.20@gmail.com>
2022-10-13 14:30:51 -0400

    solar farm pt2 complete

---
+++ week-09/exercises/solar-farm-pt2/src/App.js
@@ -0,0 +1,13 @@
+import SolarPanels from "./SolarPanels";
+
+
+function App() {
+  return (
+    <>
+      <h1>Welcome To The Solar Farm</h1>
+      <SolarPanels/>
+    </>
+  );
+}
+
+export default App;
